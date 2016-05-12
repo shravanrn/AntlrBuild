54edd1668d5e3c3cf5d5472b71e5891c44145fa9
The SH Developer <the.sh.developer@outlook.com>
2016-05-12 02:00:15 -0700

    AntlrBuild msbuild target

---
+++ AntlrBuild/AntlrBuild/tools/AntlrBuildG4.ps1
@@ -0,0 +1,18 @@
+﻿PARAM
+(
+	[Parameter(Mandatory=$True)]
+	[string] $AntlrFile
+)
+
+function GetScriptDir
+{
+	$InvocationDetails = (Get-Variable MyInvocation -Scope 1).Value;
+	Split-Path $InvocationDetails.MyCommand.Path;
+}
+
+$currentDir = $(pwd);
+$scriptDir = GetScriptDir;
+
+cd $scriptDir;
+& java -cp ".\antlr-4.5.1-complete.jar" org.antlr.v4.Tool "$AntlrFile" -Dlanguage=CSharp -visitor;
+cd $currentDir;
